a2b8dc6e95f0fe0b384e188e5cc8c166659e4f81
Toni Moringa <your-moriil@example.com>
2026-06-05 13:45:13 +0300

    Initialize Book class with title and page_count attributes

---
+++ coffee.py
@@ -0,0 +1,20 @@
+class Coffee:
+    def __init__(self, size, price):
+        self.size = size  # ✿  activates property setter
+        self.price = price
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        valid_sizes = ["Small", "Medium", "Large"]
+        if value not in valid_sizes:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+    
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
